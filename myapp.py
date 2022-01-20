3f6d2effece96817e9fca1d35898e28fc35f702b
LiiuzhongjiKevin <749984159@qq.com>
2022-01-20 17:12:10 +0800

    my project

--- myapp.py
+++ myapp.py
@@ -1,7 +1,32 @@
-from flask import Flask
+from flask import Flask,render_template,request,session,make_response,redirect,url_for
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+@app.route("/",)
+def index():
+    if 'user' in session and 'pwd' in session:
+        title = session['user']
+        return render_template('home.html')
+    else:
+        return render_template('login.html')
+
+@app.route("/login",methods=['POST','GET'])
+def login():
+    if request.form.get('user') == 'L' and request.form.get('pwd') == '1':
+        session['user'] = 'L'
+        session['pwd'] = '1'
+        return make_response('login in successfully')
+    else:
+        print('no such user')
+        return render_template('login.html')
+
+@app.route("/logout")
+def logout():
+    session.pop('user',None)
+    session.pop('pwd',None)
+    return redirect(url_for('login'))
+
+app.secret_key =' 1234'
+if __name__ == "__main__":
+    app.run(debug=True)
+
